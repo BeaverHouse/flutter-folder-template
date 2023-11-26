f2fd2f7f99a6e077ea8f1465977e967ecfbe500d
LHU <haulrest@gmail.com>
2023-11-26 16:34:07 +0900

    fix main function

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final prefs = await SharedPreferences.getInstance();
 
   runApp(ProviderScope(
